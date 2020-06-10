17541ebfbbab7b7bd08b6dc3613e2937b01cedb8
Hotrung2311 <trungho2311@gmail.com>
2020-06-10 16:27:28 +0700

    Merge branch 'master' into Trung
    # Conflicts:
    #	.idea/vcs.xml
    #	web/list.jsp

--- src/Model/Prisoner.java
+++ src/Model/Prisoner.java
@@ -3,54 +3,18 @@ package Model;
 public class Prisoner {
     private int id;
     private String cellRoom;
-    private String name;
-    private int age;
-    private double height;
-    private double weight;
-    private String address;
-
-    private String identification;
-    private String crime;
-    private String date_arrived;
-    private String date_departure;
-    private String judgment;
-    private String other;
+    private CrimeInform crimeInform;
+    private PrivateInform privateInform;
 
     public Prisoner(int id, String cellRoom) {
         this.id = id;
         this.cellRoom = cellRoom;
     }
 
-    public Prisoner(String name, int age, double height, double weight, String address, String identification) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        this.address = address;
-        this.identification = identification;
+    public Prisoner() {
+
     }
 
-    public Prisoner(String crime, String date_arrived, String date_departure, String judgment, String other) {
-        this.crime = crime;
-        this.date_arrived = date_arrived;
-        this.date_departure = date_departure;
-        this.judgment = judgment;
-        this.other = other;
-    }
-    public void  updateCrimeInform(String crime,String date_arrived,String date_departure,String judgment,String other){
-        this.crime = crime;
-        this.date_arrived = date_arrived;
-        this.date_departure = date_departure;
-        this.judgment = judgment;
-        this.other = other;
-    }
-    public  void  updatePrivateInform(String name,double height,double weight,String address,String identification ){
-        this.name = name;
-        this.height = height;
-        this.weight = weight;
-        this.address = address;
-        this.identification = identification;
-    }
     public int getId() {
         return id;
     }
@@ -67,91 +31,19 @@ public class Prisoner {
         this.cellRoom = cellRoom;
     }
 
-    public String getName() {
-        return name;
+    public CrimeInform getCrimeInform() {
+        return crimeInform;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCrimeInform(CrimeInform crimeInform) {
+        this.crimeInform = crimeInform;
     }
 
-    public int getAge() {
-        return age;
+    public PrivateInform getPrivateInform() {
+        return privateInform;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public String getIdentification() {
-        return identification;
-    }
-
-    public void setIdentification(String identification) {
-        this.identification = identification;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCrime() {
-        return crime;
-    }
-
-    public void setCrime(String crime) {
-        this.crime = crime;
-    }
-
-    public String getDate_arrived() {
-        return date_arrived;
-    }
-
-    public void setDate_arrived(String date_arrived) {
-        this.date_arrived = date_arrived;
-    }
-
-    public String getDate_departure() {
-        return date_departure;
-    }
-
-    public void setDate_departure(String date_departure) {
-        this.date_departure = date_departure;
-    }
-
-    public String getJudgment() {
-        return judgment;
-    }
-
-    public void setJudgment(String judgment) {
-        this.judgment = judgment;
-    }
-
-    public String getOther() {
-        return other;
-    }
-
-    public void setOther(String other) {
-        this.other = other;
+    public void setPrivateInform(PrivateInform privateInform) {
+        this.privateInform = privateInform;
     }
 }
